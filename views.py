bc00b1c1cfe6f0e7fe0fec718a4b1ca02e6d56e0
wmaithil <wmaithil@gmail.com>
2020-04-15 13:04:34 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -2,14 +2,12 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login , logout
 
 # Create your views here.
 def index(request):
     return render(request,'blog/index.html')
- 
-def login(request):
-    return render(request,'blog/si?name=Maithil+R+Wadehg&text=ydcdach+cudchdb+dcbgnin.html')
 
 def cover(request): 
     return render(request,'blog/cover.html')
@@ -39,6 +37,7 @@ def sunset(request):
 
 def beach(request):
     return render(request,'blog/beach.html')
+#_________________________________________________________________________________
 
 def confirm(request):
     djname=request.GET.get('name','default')
@@ -47,4 +46,56 @@ def confirm(request):
     print("{} is the received input from server".format(djtext))
     params={'djname':djname,'djtext': djtext,'djby':by}
     return render(request,'blog/mauri.html',params)
+
+#_________________________________________________________________________________
+#SignUp
+def signup(request):
+    if(request.method=="POST"):
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        passwor2 = request.POST['password2']
+        
+        # form validation 
+        if not username.isalnum():
+            messages.error(request,'atleast 1 letter and number required')
+            return redirect('blog')
+        if(passwor2!=password1):
+            messages.error(request,'Passwords do not match')
+            return redirect('blog')
+        #
+        myuser= User.objects.create_user(username,  password1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request ,"Account created Successfully")
+        return redirect('blog ')
+    else:
+        return HttpResponse("404 - Page Not found")
+
+#Login
+#____________________________________________________________________________________
+
+def handleLogin(request):
+    if(request.method=="POST"):
+        Lusername=request.POST[uname]
+        Lpassword=request.POST[passwd]
+
+        #authentication
+        user= authenticate(username=Lusername , password=Lpassword)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request,"Successfully logged in")
+    else:
+        return messages.error(request,'404')
+    return HttpResponse('handleLogin')
+
+#________________________________________________________________________________________
+#Logout
+
+def handleLogout(request):
+
+    return HttpResponse('handleLogout')
   
