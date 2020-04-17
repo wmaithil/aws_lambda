4848ef7cc41057ffe4fb951917550672aef2072d
wmaithil <wmaithil@gmail.com>
2020-04-17 19:54:21 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,18 @@
 
 from django.conf.urls import url 
-
 from . import views
 
 urlpatterns = [
-    url(r'blogpost',views.blogpost,name='blogpost'),
-    url(r'blog',views.index,name='index'),
-    url(r'login',views.login,name='login'),
+     
+    # blog_pages urls
     url(r'mauri',views.mauritius,name='mauritius'),
-    url(r'beach',views.beach,name='cover'),
+    url(r'beach',views.beach,name='beach'),
     url(r'confirm',views.confirm,name='confirm'),
-    url(r'sunset',views.sunset,name='cover'),
+    url(r'sunset',views.sunset,name='sunset'),
     url(r'home',views.home,name='home'),
-    url(r'',views.cover,name='cover')
+    #authentication urls
+    url(r'signup',views.signup,name='signup'),
+    url(r'login',views.handleLogin,name='handleLogin'),
+    url(r'logout',views.handleLogout,name='handleLogout'),
+     url(r'^$', views.blog, name='blog')  
 ]
--- views.py
+++ views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login , logout
+from django.contrib import messages
 
 # Create your views here.
-def index(request):
+def blog(request):
     return render(request,'blog/index.html')
 
 def cover(request): 
@@ -15,8 +16,6 @@ def cover(request):
 def home(request):
     return render(request,'blog/bloghome.html')
 
-def blogpost(request):
-    return render(request,'blog/blogpost.html')
 
 def mauritius(request):
     if (request.method=="POST"):
@@ -50,25 +49,33 @@ def confirm(request):
 def signup(request):
     if(request.method=="POST"):
         fname = request.POST['fname']
+        print(fname)
         lname = request.POST['lname']
+        print(lname)
         username = request.POST['username']
+        print(username)
         password1 = request.POST['password1']
-        passwor2 = request.POST['password2']
-        
+        password2 = request.POST['password2']
+
         # form validation 
         if not username.isalnum():
             messages.error(request,'atleast 1 letter and number required')
+            print("alnum condition checked")
             return redirect('blog')
-        if(passwor2!=password1):
+
+        if(password2 != password1):
+            print("password condition checked")
             messages.error(request,'Passwords do not match')
+            #return redirect('blog')
             return redirect('blog')
         #
         myuser= User.objects.create_user(username,  password1)
         myuser.first_name = fname
         myuser.last_name = lname
         myuser.save()
+        print("user created")
         messages.success(request ,"Account created Successfully")
-        return redirect('blog ')
+        return redirect('blog')
     else:
         return HttpResponse("404 - Page Not found")
 
@@ -76,17 +83,22 @@ def signup(request):
 #____________________________________________________________________________________
 
 def handleLogin(request):
-    if(request.method=="POST"):
-        Lusername=request.POST[uname]
-        Lpassword=request.POST[passwd]
+    if request.method == "POST":
+        Lusername=request.POST['uname']
+        Lpassword=request.POST['passwd']
+        print("username recieved")
 
         #authentication
         user= authenticate(username=Lusername , password=Lpassword)
+        print("Authentication done")
 
         if user is not None:
             login(request, user)
             messages.success(request,"Successfully logged in")
+            print("Successfully logged in")
+            return redirect('blog')
         else:
+            print("authentication issues")
             messages.error(request,'Invalid credentials')
             return redirect('blog')
 
@@ -96,8 +108,9 @@ def handleLogin(request):
 #Logout
 
 def handleLogout(request):
-    if(request.method=="POST"):
+    if(request.method == "POST"):
         logout(request)
         messages.success(request,"Successfully logged out")
+        return redirect('blog') 
     return HttpResponse('handleLogout')
   
