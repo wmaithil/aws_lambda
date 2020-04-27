74095aa85facc3c4afc99ad6a50049a2143c9c12
wmaithil <wmaithil@gmail.com>
2020-04-27 22:47:05 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,18 +1,17 @@
 
 from django.conf.urls import url 
-from . import views
+from . import views 
 
 urlpatterns = [
-     
     # blog_pages urls
     url(r'mauri',views.mauritius,name='mauritius'),
     url(r'beach',views.beach,name='beach'),
-    url(r'confirm',views.confirm,name='confirm'),
     url(r'sunset',views.sunset,name='sunset'),
     url(r'home',views.home,name='home'),
+
     #authentication urls
     url(r'signup',views.signup,name='signup'),
-    url(r'login',views.handleLogin,name='handleLogin'),
+    url(r'djlogin',views.handleLogin,name='handleLogin'),
     url(r'logout',views.handleLogout,name='handleLogout'),
-     url(r'^$', views.blog, name='blog')  
+    url(r'^$', views.blog, name='blog')  
 ]
